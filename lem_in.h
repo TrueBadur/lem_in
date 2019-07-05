034ec6d00a4239662d6c34301c1f58ae85282f23
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2019-07-05 19:11:53 +0300

    Lay out main.c; Add mngr structure

--- lem_in.h
+++ lem_in.h
@@ -6,12 +6,14 @@
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/05 17:46:43 by ehugh-be          #+#    #+#             */
-/*   Updated: 2019/07/05 17:46:43 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2019/07/05 18:22:08 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEM_IN_LEM_IN_H
 #define LEM_IN_LEM_IN_H
+
+# include "libft.h"
 
 struct s_node;
 
@@ -28,5 +30,16 @@ typedef struct s_node
 	char		*name;
 	t_edge		**links;
 }				t_node;
+
+typedef struct	s_mngr
+{
+	int flags; //TODO convert to more flags or to bitarray
+}				t_mngr;
+
+void	*parse_flags(int argc, char **argv, t_mngr *mngr);
+void	parse_input(t_mngr *mngr);
+t_mngr	*init_mngr(void);
+void	make_magic(t_mngr *mngr);
+void	print_solution(t_mngr *mngr);
 
 #endif //LEM_IN_LEM_IN_H
--- main.c
+++ main.c
@@ -6,11 +6,21 @@
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/05 17:12:33 by ehugh-be          #+#    #+#             */
-/*   Updated: 2019/07/05 17:12:33 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2019/07/05 18:22:08 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "lem_in.h"
+
 int main(int argc, char **argv)
 {
+	t_mngr *mngr;
+
+	mngr = init_mngr(); //TODO
+	if (argc > 1)
+		parse_flags(argc, argv, mngr); //TODO
+	parse_input(mngr); //TODO
+	make_magic(mngr); //TODO
+	print_solution(mngr); //TODO
 	return (0);
 }
