a0325647277e501534b1aff388b334a2131dc534
mbartole <gerus66@gmail.com>
2019-07-08 03:31:28 +0300

    t_node: add /path/

--- lem_in.h
+++ lem_in.h
@@ -6,7 +6,7 @@
 /*   By: mbartole <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/05 17:46:43 by ehugh-be          #+#    #+#             */
-/*   Updated: 2019/07/07 18:13:31 by mbartole         ###   ########.fr       */
+/*   Updated: 2019/07/08 01:28:40 by mbartole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 #define LEM_IN_LEM_IN_H
 
 # include "libft.h"
+# include "pque.h"
+#include <stdio.h> // TODO: delete
 
 struct s_node;
 
@@ -21,13 +23,14 @@ typedef struct	s_edge
 {
 	struct s_node	*node1;
 	struct s_node	*node2;
-	int 			wgth12;
-	int 			wgth21;
+	int 			wgth12;  // -1 by default
+	int 			wgth21;  // -1 by default
 }				t_edge;
 
 typedef struct s_node
 {
-	int			wgth;
+	int			counter; // -1 by default
+	t_edge		*path;  // NULL by default
 	char		*name;
 	t_list		*links;  // t_edge
 }				t_node;
