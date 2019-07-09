b1ce0b672f56417be5ca3c8be339aa3f21af7854
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2019-07-09 19:16:32 +0300

    Continue writing main part of input parser

--- parse_input.c
+++ parse_input.c
@@ -6,40 +6,17 @@
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/05 18:58:51 by ehugh-be          #+#    #+#             */
-/*   Updated: 2019/07/07 21:49:21 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2019/07/09 19:09:26 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-int		get_ants_num(t_mngr *mngr, char *line)
-{
-	char *tmp;
 
-	tmp = line;
-	mngr->ant_num = ft_atoi_m((const char **)&line);
-	if (mngr->ant_num > 0 && *line == '\0')
-	{
-		free(tmp);
-		return (1);
-	}
-	free(tmp);
-	ultimate_exit(mngr);
-	return (0);
-}
 
-void get_room(t_mngr *mngr, char *line)
+t_elt	get_link(t_mngr *mngr, char *line)
 {
 
-}
-void get_link(t_mngr *mngr, char *line)
-{
-
-}
-
-t_elt	check_line_type(t_mngr * mngr, char *line)
-{
-	return (ERROR);
 }
 
 void get_instruction(t_mngr *mngr, char *line)
@@ -54,17 +31,20 @@ void parse_input(t_mngr *mngr)
 
 	while(get_next_line(STDIN_FILENO, &line) > 0)
 	{
-		type = check_line_type(mngr, line);
+		type = check_line_type(line);
 		if (type == LINK)
 			get_link(mngr, line);
 		else if (type == ROOM)
-			get_room(mngr, line);
+			type = get_room(mngr, line);
 		else if (type == INSTRUCTION)
 			get_instruction(mngr, line);
 		else if (type == ANT_N)
-			get_ants_num(mngr, line);
-		else if (type == ERROR)
+			mngr->ant_num = ft_atoi(line);
+		free(line);
+		if (type == ERROR)
 			break ;
 	}
+	if (!isenoughdata(mngr))
+		ultimate_exit(&mngr);
 }
 
