fdbe5d056667196c98651973c7d646548184f95b
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2019-07-09 20:34:58 +0300

    Fix get_room: add setting of start and finish

--- get_room.c
+++ get_room.c
@@ -6,7 +6,7 @@
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/09 19:11:10 by ehugh-be          #+#    #+#             */
-/*   Updated: 2019/07/09 19:11:28 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2019/07/09 19:13:18 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ t_elt	get_room(t_mngr *mngr, char *line)
 	if (!(node->name = get_node_name(line)))
 		ultimate_exit(&mngr);
 	node->links = NULL;
-	node->wgth = -1;
+	node->counter = -1;
 	if (ft_avlsearch(mngr->all_rooms, node->name, 0)) //TODO adapt avl to string keys and compare functions
 	{
 		free(node->name);
@@ -43,5 +43,9 @@ t_elt	get_room(t_mngr *mngr, char *line)
 		return (ERROR);
 	}
 	ft_avlins(mngr->all_rooms, ft_avlnew(node, node->name, sizeof(t_node)));
+	if (mngr->instr == START)
+		mngr->start = node;
+	if (mngr->instr == FINISH)
+		mngr->end = node;
 	return (ROOM);
 }
