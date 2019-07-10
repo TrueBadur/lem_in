fa65500768283ffc99c21582a7445bf5c4c6d1ee
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2019-07-10 15:58:24 +0300

    Add init_mngr func and comment out for tests
    Took 2 minutes

--- main.c
+++ main.c
@@ -12,15 +12,38 @@
 
 #include "lem_in.h"
 
+static t_mngr	*init_mngr(void)
+{
+	t_mngr *ret;
+
+	if (!(ret = malloc(sizeof(t_mngr))))
+		return (NULL);
+	ft_bzero(ret, sizeof(t_mngr));
+	ret->max_lt = LT_NONE;
+	ret->ant_num = -1;
+	ret->instr = INSTR_NONE;
+	return (ret);
+}
+
+//TODO remove placeholder
+void	ultimate_exit(t_mngr **mngr)
+{
+	*mngr = NULL;
+	exit(1);
+}
+
 int main(int argc, char **argv)
 {
 	t_mngr *mngr;
 
-	mngr = init_mngr(); //TODO
-	if (argc > 1)
-		parse_flags(argc, argv, mngr); //TODO
+	if (!(mngr = init_mngr()))
+		ultimate_exit(NULL);//TODO
+	argc += 0;
+	argv += 0;
+//	if (argc > 1)
+//		parse_flags(argc, argv, mngr); //TODO
 	parse_input(mngr); //TODO
-	make_magic(mngr); //TODO
-	print_solution(mngr); //TODO
-	return (0);
+//	make_magic(mngr); //TODO
+//	print_solution(mngr); //TODO
+//	return (0);
 }
